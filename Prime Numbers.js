5a6c3d4444bf0937397df495a6b1c5c03ada7592
Adham El Banhawy <the.benhawy@gmail.com>
2016-12-17 20:06:58 -0600

    Efficient Prime
    Added efficient prime number function

--- Prime Numbers.js
+++ Prime Numbers.js
@@ -24,4 +24,26 @@ function isPrime(num, num2){
 	}
 }
 
-console.log(isPrime(0, 100));
+function numberOfPrimes(arr)
+{
+	return arr.length;
+}
+
+function efficientPrime(num, num2){  //This function is much more efficient
+	if (num <= 1)
+	{
+		return false;
+	}
+	for (var i = 2; i <= Math.sqrt(num); i++) {
+		if (num%i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+console.log(isPrime(1));
+console.log(efficientPrime(1));
+
+//console.log(Math.pow(2,4));
