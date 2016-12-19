f969fecbdec73034afd9dd84abef932ae27b58ba
Adham El Banhawy <the.benhawy@gmail.com>
2016-12-18 21:39:14 -0600

    Checking for a Sum Pair
    Wrote 2 different functions to check if a pair of numbers within array
    add up to a passed sum

---
+++ Google Challenges/CHeck Num Pair in Array Equals Sum.js
@@ -0,0 +1,37 @@
+//Write a function that takes in an array of numbers and sum and checks if there is a pair of numbers within the array that adds up to the sum
+//Assume the array passed is sorted in ascending order
+
+function checkPairSum(arr, num){
+	for (var i= 0; i < arr.length; i++){
+		for (var j= i+1; j<arr.length; j++)
+		if (arr[i]+arr[j] == num){
+			return true;
+		}
+	}
+	return false;
+} 
+
+function efficientCheckPairSum(arr, num){
+	var high = arr.length -1;
+	var low = 0;
+
+	while (low<high){
+		var pair= arr[low] + arr[high];
+		if ( pair === num){
+			return true;
+		}else if(pair < num){
+			low++;
+		}else {
+			high--
+		}
+	}
+	return false;
+}
+
+//Inefficient way
+console.log(checkPairSum([1,2,3,9], 8));
+console.log(checkPairSum([1,2,4,4], 8));
+
+//slightly more efficient way
+console.log(efficientCheckPairSum([1,2,3,9], 8));
+console.log(efficientCheckPairSum([1,2,4,4], 8));
